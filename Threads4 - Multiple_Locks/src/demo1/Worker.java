68db850ff392e9409e682e2e161168b69a7de7d3
Akshay P <akshay.py@gmail.com>
2016-06-09 17:06:25 +0700

    comments added

--- Threads4 - Multiple_Locks/src/demo1/Worker.java
+++ Threads4 - Multiple_Locks/src/demo1/Worker.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+// Single Intrinsic Lock - Separate methods, separate threads
 public class Worker {
 
 	private final Random random = new Random();
--- Threads4 - Multiple_Locks/src/demo2/Worker.java
+++ Threads4 - Multiple_Locks/src/demo2/Worker.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
+// Multiple Locks - Separate methods, separate threads
 public class Worker {
 
 	private final Random random = new Random();
